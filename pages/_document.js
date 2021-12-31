51955ec075fa13cf16000cd7694c0181fa2a7385
thiagosullivan <thiagosullivan12@gmail.com>
2021-12-31 04:44:16 -0300

    Adding new meta tags for SEO

--- pages/_document.js
+++ pages/_document.js
@@ -37,11 +37,28 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          {/* <!-- Primary Meta Tags --> */}
+          <meta name="title" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+          <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."/>
+
+          {/* <!-- Open Graph / Facebook --> */}
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://anatomia-palpatoria.vercel.app/"/>
+          <meta property="og:title" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."/>
+          <meta property="og:description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."/>
+          <meta property="og:image" content="https://i.ibb.co/QCz7Qx4/anatomia-logo-meta-tag.png"/>
+
+          {/* <!-- Twitter --> */}
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://anatomia-palpatoria.vercel.app/"/>
+          <meta property="twitter:title" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."/>
+          <meta property="twitter:description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit."/>
+          <meta property="twitter:image" content="https://i.ibb.co/QCz7Qx4/anatomia-logo-meta-tag.png"></meta>
         </Head>
         <body>
           <Main />
