d8ca2bac2bd42d599fcd787b818dae92d2ba1293
thiagosullivan <thiagosullivan12@gmail.com>
2021-12-31 03:55:01 -0300

    Changing Scroll to Top component

--- components/ScrollToTop/index.js
+++ components/ScrollToTop/index.js
@@ -3,34 +3,52 @@ import { ScrollContainer } from './styles';
 import { IoIosArrowUp } from 'react-icons/io';
 
 function ScrollToTop() {
-  const [isVisible, setIsVisible] = useState(true);
-  const [height, setHeight] = useState(0)
-  
-  useEffect(() => {   
-    window.addEventListener("scroll", listenToScroll);
-    return () => 
-       window.removeEventListener("scroll", listenToScroll); 
-  }, [])
-  
-  const listenToScroll = () => {
-    let heightToShowFrom = 200;
-    const winScroll = document.body.scrollTop || 
-        document.documentElement.scrollTop;
-    setHeight(winScroll);
+  const [showButton, setShowButton] = useState(false);
 
-    if (winScroll > heightToShowFrom) {  
-         isVisible && setIsVisible(true);
-    } else {
-         setIsVisible(false);
-    }  
-  };
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 200) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  }
+
+  // const [isVisible, setIsVisible] = useState(true);
+  // const [height, setHeight] = useState(0)
+
+  // const listenToScroll = () => {
+  //   let heightToShowFrom = 200;
+  //   const winScroll = document.body.scrollTop || 
+  //       document.documentElement.scrollTop;
+  //   setHeight(winScroll);
+
+  //   if (winScroll > heightToShowFrom) {  
+  //        isVisible && setIsVisible(true);
+  //   } else {
+  //        setIsVisible(false);
+  //   }  
+  // };
+  
+  // useEffect(() => {   
+  //   window.addEventListener("scroll", listenToScroll);
+  //   return () => 
+  //      window.removeEventListener("scroll", listenToScroll); 
+  // }, [])
+  
+  
 
   return (
     <>
       {
-        isVisible 
+        showButton 
         &&
-        <ScrollContainer onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+      <ScrollContainer onClick={scrollToTop}>
         <IoIosArrowUp />
       </ScrollContainer>
       }
