9f550cd41c397b733268bc19e1ecf4b31ed9fd9c
thiagosullivan <thiagosullivan12@gmail.com>
2022-01-07 11:18:46 -0300

    Corrections for deploy #5

--- components/Terms/index.js
+++ components/Terms/index.js
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 function Terms() {
   return (
@@ -11,7 +11,7 @@ function Terms() {
         <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
         <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
         <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
-        <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <Link href='https://politicaprivacidade.com' target='_BLANK'>políticas de privacidade</Link>.</p>
+        <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</p>
         <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados</p>
         <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
 
@@ -51,9 +51,9 @@ function Terms() {
           <h3>Compromisso do Usuário</h3>
           <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Anatomia Palpatória Avançada oferece no site e com caráter enunciativo, mas não limitativo:</p>
           <ul>
-            <li>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</li>
-            <li>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, <Link href='https://jogoshoje.io'>jogos de hoje</Link> ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li>
-            <li>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Anatomia Palpatória Avançada, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</li>
+            <li>A&#x29; Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</li>
+            <li>B&#x29; Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, jogos de hoje ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li>
+            <li>C&#x29; Não causar danos aos sistemas físicos &#x28;hardwares&#x29; e lógicos &#x28;softwares&#x29; do Anatomia Palpatória Avançada, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</li>
             </ul>
             <h3>Mais informações</h3>
             <p>Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.</p>
@@ -65,12 +65,12 @@ function Terms() {
         <p>Ao acessar ao site Anatomia Palpatória Avançada, concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis ​​e concorda que é responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e marcas comerciais aplicáveis.</p>
 
         <h3>2. Uso de Licença</h3>
-        <p>É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou software) no site Anatomia Palpatória Avançada , apenas para visualização transitória pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode:<br/>
+        <p>É concedida permissão para baixar temporariamente uma cópia dos materiais &#x28;informações ou software&#x29; no site Anatomia Palpatória Avançada , apenas para visualização transitória pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta licença, você não pode:<br/>
         modificar ou copiar os materiais;
         </p>
 
         
-        <p>usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial ou não comercial);
+        <p>usar os materiais para qualquer finalidade comercial ou para exibição pública &#x28;comercial ou não comercial&#x29; ;
         tentar descompilar ou fazer engenharia reversa de qualquer software contido no site Anatomia Palpatória Avançada;
         remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou transferir os materiais para outra pessoa ou &apos;espelhe&apos; os materiais em qualquer outro servidor.<br/>
         Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá ser rescindida por Anatomia Palpatória Avançada a qualquer momento. Ao encerrar a visualização desses materiais ou após o término desta licença, você deve apagar todos os materiais baixados em sua posse, seja em formato eletrónico ou impresso.</p>
@@ -80,7 +80,7 @@ function Terms() {
         Além disso, o Anatomia Palpatória Avançada não garante ou faz qualquer representação relativa à precisão, aos resultados prováveis ​​ou à confiabilidade do uso dos materiais em seu site ou de outra forma relacionado a esses materiais ou em sites vinculados a este site.</p>
         
         <h3>4. Limitações</h3>
-        <p>Em nenhum caso o Anatomia Palpatória Avançada ou seus fornecedores serão responsáveis ​​por quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais em Anatomia Palpatória Avançada, mesmo que Anatomia Palpatória Avançada ou um representante autorizado da Anatomia Palpatória Avançada tenha sido notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações de responsabilidade por danos conseqüentes ou incidentais, essas limitações podem não se aplicar a você.</p>
+        <p>Em nenhum caso o Anatomia Palpatória Avançada ou seus fornecedores serão responsáveis ​​por quaisquer danos &#x28;incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos negócios&#x29; decorrentes do uso ou da incapacidade de usar os materiais em Anatomia Palpatória Avançada, mesmo que Anatomia Palpatória Avançada ou um representante autorizado da Anatomia Palpatória Avançada tenha sido notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdições não permitem limitações em garantias implícitas, ou limitações de responsabilidade por danos conseqüentes ou incidentais, essas limitações podem não se aplicar a você.</p>
 
         <h3>5. Precisão dos materiais</h3>
         <p>Os materiais exibidos no site da Anatomia Palpatória Avançada podem incluir erros técnicos, tipográficos ou fotográficos. Anatomia Palpatória Avançada não garante que qualquer material em seu site seja preciso, completo ou atual. Anatomia Palpatória Avançada pode fazer alterações nos materiais contidos em seu site a qualquer momento, sem aviso prévio. No entanto, Anatomia Palpatória Avançada não se compromete a atualizar os materiais.</p>
