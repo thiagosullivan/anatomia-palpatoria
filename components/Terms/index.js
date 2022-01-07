fdce5abe82f52f2f641e64ffce2322be821f22fc
thiagosullivan <thiagosullivan12@gmail.com>
2022-01-07 11:07:57 -0300

    Corrections for deploy #4

--- components/Terms/index.js
+++ components/Terms/index.js
@@ -7,11 +7,11 @@ function Terms() {
       <div className='terms'>
         <h2 id="politica">Política Privacidade</h2>
 
-        <p>A sua privacidade é importante para nós. É política do Anatomia Palpatória Avançada respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <Link href='https://anatomiapalpatoriaavancada.com.br'><a>Anatomia Palpatória Avançada</a></Link>, e outros sites que possuímos e operamos.</p>
+        <p>A sua privacidade é importante para nós. É política do Anatomia Palpatória Avançada respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <Link href='https://anatomiapalpatoriaavancada.com.br'>Anatomia Palpatória Avançada</Link>, e outros sites que possuímos e operamos.</p>
         <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
         <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
         <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
-        <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <Link href='https://politicaprivacidade.com' target='_BLANK'><a>políticas de privacidade</a></Link>.</p>
+        <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <Link href='https://politicaprivacidade.com' target='_BLANK'>políticas de privacidade</Link>.</p>
         <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados</p>
         <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
 
@@ -52,7 +52,7 @@ function Terms() {
           <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Anatomia Palpatória Avançada oferece no site e com caráter enunciativo, mas não limitativo:</p>
           <ul>
             <li>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</li>
-            <li>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, <Link href='https://jogoshoje.io'><a>jogos de hoje</a></Link> ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li>
+            <li>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, <Link href='https://jogoshoje.io'>jogos de hoje</Link> ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</li>
             <li>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Anatomia Palpatória Avançada, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</li>
             </ul>
             <h3>Mais informações</h3>
