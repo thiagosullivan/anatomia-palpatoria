aba27033f0b47fbbdbce5b83ff75041a6eb7b647
thiagosullivan <thiagosullivan12@gmail.com>
2022-03-17 16:09:48 -0300

    changing developer company on footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer(){
         <p>&quot;Esse produto é comercializado com apoio da Hotmart. A plataforma não faz controle editorial prévio dos produtos comercializados, nem avalia a tecnicidade e experiência daqueles que os produzem. A existência de um produto e sua aquisição, por meio da plataforma, não podem ser consideradas como garantia de qualidade de conteúdo e resultado, em qualquer hipótese. Ao adquiri-lo, o comprador declara estar ciente dessas informações. Os termos e políticas da Hotmart podem ser acessados aqui, antes mesmo da conclusão da compra.&quot;</p>
       </FooterTop>
       <FooterBottom>
-        <p>© 2022 <strong>Anatomia Palpatória Avançada</strong> - Desenvolvido por <strong>Eagle</strong>.</p>
+        <p>© 2022 <strong>Anatomia Palpatória Avançada</strong> - Desenvolvido por <strong>Digital Cloud Brasil</strong>.</p>
       </FooterBottom>
     </FooterContainer>
   )
