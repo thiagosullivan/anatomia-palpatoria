f8edebc4b56dc58bf556b3afdfeb65436f5e7b4c
thiagosullivan <thiagosullivan12@gmail.com>
2022-04-08 11:32:40 -0300

    changing infos on questions section

--- services/dataquestions.js
+++ services/dataquestions.js
@@ -1,38 +1,22 @@
 export const Data = [
   {
     question: 'A cobrança será mensal?',
-    answer: 'Sim. O sistema de assinatura é feito por meio de cartão de crédito e a cobrança será mensal. Não se preocupe, não é necessário ter um limite maior que o valor da parcela.',
-  },
-  {
-    question: 'Posso pagar o valor integral do curso?',
-    answer: 'Para que você tenha maior liberdade no tempo de estudo e não fique pressionado(a) a uma data de conclusão, o curso foi criado com o sistema de assinaturas. Assim, você pagará de forma mensal apenas até a data de conclusão. Ou seja, o quanto antes você concluir o curso, antes você termina o seu pagamento.',
-  },
-  {
-    question: 'Existe algum desconto em um plano anual?',
-    answer: 'Ainda não temos um plano anual, apenas mensal. Mas se existir uma demanda organizamos um novo projeto.',
+    answer: 'Não. O pagamento do curso é único e permite o seu acesso por 12 meses na plataforma.',
   },
   {
     question: 'Em qual plataforma eu terei acesso ao material?',
     answer: 'O curso está hospedado na plataforma Hotmart. Você receberá em seu e-mail o link para acesso ao curso. Se você já tem cadastro na Hotmart e usou o mesmo e-mail para compra então você usará o mesmo login e senha para acesso.',
   },
   {
-    question: 'Eu continuo tendo acesso ao material mesmo depois do cancelamento da assinatura?',
-    answer: 'Não. O cancelamento exclui automaticamente o acesso ao curso. Por ser um valor bastante acessível, sugiro que mesmo após o término do curso você mantenha sua assinatura ativa por no mínimo 2 meses. Assim você terá acesso em 100% do curso para consultas, tirar dúvida ou assistir novas aulas gratuitas com atualização de conteúdos.',
+    question: 'Por quanto tempo continuo tendo acesso ao material mesmo depois de cancelar minha compra?',
+    answer: 'No caso de cancelamento da compra o acesso é automaticamente removido pela plataforma na hotmart.',
   },
   {
     question: 'Quais são as formas de pagamentos do curso?',
-    answer: 'O pagamento é feito exclusivamente por meio de cartão de crédito.',
+    answer: 'O pagamento pode ser feito por meio de cartão de crédito ou boleto bancário.',
   },
   {
     question: 'Qual o valor da taxa de cancelamento?',
-    answer: 'Não é cobrada nenhuma taxa de cancelamento. Você pode cancelar no momento que quiser. Mas lembre-se, ao cancelar sua compra será cancelado também o acesso ao curso. Este é um padrão no sistema de assinaturas.',
-  },
-  {
-    question: 'Qual a duração do curso?',
-    answer: 'O bacana do formato de assinatura é que o curso terá a duração que você levar para concluir. Ou seja, você não tem pressão de tempo para concluir, o ritmo é o seu.',
-  },
-  {
-    question: 'Haverá algum tipo de suporte/consultoria com o professor do curso?',
-    answer: 'Sim. Teremos nossos canais extras de comunicação. Além dos comentários nos vídeos das aulas, teremos: canal exclusivo no Telegram e comunidade privada no Facebook, nos quais além das considerações, todos os alunos poderão levar dúvidas e responder dúvidas dos colegas.',
+    answer: 'Não é cobrada nenhuma taxa de cancelamento. Você pode cancelar no momento que quiser. Mas lembre-se, ao cancelar sua compra será cancelado também o acesso ao curso.',
   }
 ]
